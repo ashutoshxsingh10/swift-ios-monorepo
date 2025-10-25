3c1844b512b598332860b7b58ffc59c06aeb44b5
ashutoshxsingh10 <ashutoshxsingh10@gmail.com>
2025-10-25 23:16:37 +0530

    Initial Commit

---
+++ CursorTracking/CursorTrackingApp.swift
@@ -0,0 +1,19 @@
+//
+// CursorTrackingApp.swift
+// CursorTracking
+//
+// Created by Ashutosh Kumar Singh on 25/10/25.
+//
+
+import SwiftUI
+
+@main
+struct CursorTrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+//made in india
+
